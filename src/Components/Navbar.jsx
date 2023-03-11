29dd944eef7dcb192fdcc141b58398a32a483417
SarojPahi <107534043+SarojPahi@users.noreply.github.com>
2023-03-11 10:02:04 +0530

    Your bets page created

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import dynamic from "next/dynamic";
+import Link from "next/link";
 
 const WalletMultiButtonDynamic = dynamic(
   async () =>
@@ -9,6 +10,11 @@ const Navbar = () => {
   return (
     <div className="flex justify-between w-full items-center h-14 border-gray-200 border-b-2 px-4">
       <div className="text-red-500">Logo</div>
+      <Link href={"/Bets"}>
+        <div className="text-red-700 tracking-wider transition-all duration-150">
+          Your Bets
+        </div>
+      </Link>
       <div>
         <WalletMultiButtonDynamic
           style={{
